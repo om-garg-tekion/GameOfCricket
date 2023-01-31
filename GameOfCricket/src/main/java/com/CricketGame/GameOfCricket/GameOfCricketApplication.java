4f3d2919c08e7a0c1373e951e6ebd4e6cebf9843
om-garg-tekion <123242450+om-garg-tekion@users.noreply.github.com>
2023-01-31 22:54:43 +0530

    Add files via upload

---
+++ GameOfCricket/src/main/java/com/CricketGame/GameOfCricket/GameOfCricketApplication.java
@@ -0,0 +1,13 @@
+package com.CricketGame.GameOfCricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfCricketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfCricketApplication.class, args);
+	}
+
+}
---
+++ GameOfCricket/src/test/java/com/CricketGame/GameOfCricket/GameOfCricketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CricketGame.GameOfCricket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfCricketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
