bd77d78e1c92fc4656aacec67724b2d3d20a97ef
om-garg-tekion <ogarg@tekion.com>
2023-02-28 12:40:59 +0530

    Made constructor of Constants class private

--- src/main/java/com/CricketGame/GameOfCricket/constants/Constants.java
+++ src/main/java/com/CricketGame/GameOfCricket/constants/Constants.java
@@ -1,6 +1,7 @@
 package com.CricketGame.GameOfCricket.constants;
 
 public class Constants {
+    private Constants(){}
     public static final int BALLS_IN_A_OVER = 6;
 
     public static final int START_BALLS_IN_A_OVER = 0;
