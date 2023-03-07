8da25a453bc8a028d4f86ff011fdaca8aaf09295
om-garg-tekion <ogarg@tekion.com>
2023-03-07 12:02:04 +0530

    Added validation for different teams

--- src/main/java/com/CricketGame/GameOfCricket/service/validator/MatchValidator.java
+++ src/main/java/com/CricketGame/GameOfCricket/service/validator/MatchValidator.java
@@ -4,6 +4,8 @@ import com.CricketGame.GameOfCricket.model.beans.Match;
 import com.CricketGame.GameOfCricket.model.beans.player.Player;
 import com.CricketGame.GameOfCricket.service.dataAccessService.AllService;
 
+import java.util.Objects;
+
 public class MatchValidator {
     public static boolean inputValidator(Match match){ // change name to validator
         if (!AllService.teamService.validateTeam(match.getFirstTeam().getId(), match.getId())) {
@@ -21,6 +23,9 @@ public class MatchValidator {
             if (AllService.playerService.validatePlayer(player.getId(), match.getSecondTeam().getId(), match.getId())) {
                 return true;
             }
+        }
+        if(Objects.equals(match.getFirstTeam().getId(), match.getSecondTeam().getId())){
+            return true;
         }
         return false;
     }
