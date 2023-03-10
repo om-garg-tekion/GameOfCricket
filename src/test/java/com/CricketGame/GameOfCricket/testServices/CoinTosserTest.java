3482b6d53227cb1037eec7ce2b92df305e741fde
om-garg-tekion <ogarg@tekion.com>
2023-03-10 08:51:01 +0530

    Tested coinTosser class

---
+++ src/test/java/com/CricketGame/GameOfCricket/testServices/CoinTosserTest.java
@@ -0,0 +1,32 @@
+package com.CricketGame.GameOfCricket.testServices;
+
+
+import com.CricketGame.GameOfCricket.model.enums.Coin;
+import com.CricketGame.GameOfCricket.service.coinTosser.CoinTosser;
+import com.CricketGame.GameOfCricket.service.singletonInstantiator.RandomNumberGenerator;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+public class CoinTosserTest {
+
+    @InjectMocks
+    CoinTosser coinTosser;
+
+    @Mock
+    RandomNumberGenerator randomNumberGenerator;
+
+    @Test
+    public void testCoinTossing(){
+        when(randomNumberGenerator.nextBoolean()).thenReturn(false);
+        Assert.assertNotEquals(Coin.HEADS, coinTosser.flip());
+    }
+
+}
